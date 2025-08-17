0a8a4de572afa8296d97effca539a3e38469daeb
JASWANTH <janajaswanth2006@gmail.com>
2025-08-17 16:18:30 +0530

    login

--- includes/header.php
+++ includes/header.php
@@ -1,8 +1,9 @@
 <!-- Start of header section
-	============================================= -->
+    ============================================= -->
+    
 <header id="ori-header" class="ori-header-section header-style-one">
-	<div class="ori-header-content-area">
-		<div class="ori-header-content d-flex align-items-center justify-content-between">
+    <div class="ori-header-content-area">
+        <div class="ori-header-content d-flex align-items-center justify-content-between">
             <div class="brand-logo">
                 <a href="index">
                     <picture>
@@ -11,168 +12,167 @@
                     </picture>
                 </a>
             </div>
-			<div class="ori-main-navigation-area">
-				<nav class="ori-main-navigation  clearfix ul-li">
-					<ul id="main-nav" class="nav navbar-nav clearfix">
-						<li><a href="index">Home</a></li>
-						<li>
-							<a href="showverse">Showverse</a>
-						</li>
-						<li >
-							<a href="app">App</a>
-						</li>
-						<li class="dropdown">
-							<a href="events">Events</a>
-							<ul class="dropdown-menu clearfix">
-								<li><a href="upcoming">Upcoming</a></li>
-								<li><a href="past-highlights">Past Highlights</a></li>
-							</ul>
-						</li>
-						<li class="dropdown">
-							<a href="opportunities">Opportunities</a>
-							<ul class="dropdown-menu clearfix">
-								<li><a href="models">Models</a></li>
-								<li><a href="designers">Designers</a></li>
-								<li><a href="makeup-artists">Makeup Artists</a></li>
-								<li><a href="sponsors">Sponsors</a></li>
-							</ul>
-						</li>
-						<li class="dropdown">
-							<a href="media">Media</a>
-							<ul class="dropdown-menu clearfix">
-								<li><a href="press">Press & Bytes</a></li>
-								<li><a href="gallery">Gallery</a></li>
-								<li><a href="blogs">Blogs</a></li>
-								<li><a href="contact">Get in Touch</a></li>
-							</ul>
-						</li>
-					</ul>
-				</nav>
-			</div>
-			<div class="ori-header-sidebar-search d-flex align-items-center">
-				<div class="ori-sidenav-btn navSidebar-button">
-					<button><i class="fal fa-bars"></i></button>
-				</div>
-			</div>
-		</div>
-		<div class="mobile_menu position-relative">
-			<div class="mobile_menu_button open_mobile_menu">
-				<i class="fal fa-bars"></i>
-			</div>
-			<div class="mobile_menu_wrap">
-				<div class="mobile_menu_overlay open_mobile_menu"></div>
-				<div class="mobile_menu_content">
-					<div class="mobile_menu_close open_mobile_menu">
-						<i class="fal fa-times"></i>
-					</div>
+            <div class="ori-main-navigation-area">
+                <nav class="ori-main-navigation  clearfix ul-li">
+                    <ul id="main-nav" class="nav navbar-nav clearfix">
+                        <li><a href="index">Home</a></li>
+                        <li><a href="showverse">Showverse</a></li>
+                        <li><a href="app">App</a></li>
+                        <li class="dropdown">
+                            <a href="events">Events</a>
+                            <ul class="dropdown-menu clearfix">
+                                <li><a href="upcoming">Upcoming</a></li>
+                                <li><a href="past-highlights">Past Highlights</a></li>
+                            </ul>
+                        </li>
+                        <li class="dropdown">
+                            <a href="opportunities">Opportunities</a>
+                            <ul class="dropdown-menu clearfix">
+                                <li><a href="models">Models</a></li>
+                                <li><a href="designers">Designers</a></li>
+                                <li><a href="makeup-artists">Makeup Artists</a></li>
+                                <li><a href="sponsors">Sponsors</a></li>
+                            </ul>
+                        </li>
+                        <li class="dropdown">
+                            <a href="media">Media</a>
+                            <ul class="dropdown-menu clearfix">
+                                <li><a href="press">Press & Bytes</a></li>
+                                <li><a href="gallery">Gallery</a></li>
+                                <li><a href="blogs">Blogs</a></li>
+                                <li><a href="contact">Get in Touch</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+
+            <!-- Header Right Side -->
+            <div class="ori-header-sidebar-search d-flex align-items-center">
+                <!-- ✅ Login Button (Will hide when logged in) -->
+                <a href="app" class="login-btn" id="loginBtn">Login / Signin</a>
+
+                <div class="ori-sidenav-btn navSidebar-button">
+                    <button><i class="fal fa-bars"></i></button>
+                </div>
+            </div>
+        </div>
+        
+        <!-- Mobile Menu -->
+        <div class="mobile_menu position-relative">
+            <div class="mobile_menu_button open_mobile_menu">
+                <i class="fal fa-bars"></i>
+            </div>
+            <div class="mobile_menu_wrap">
+                <div class="mobile_menu_overlay open_mobile_menu"></div>
+                <div class="mobile_menu_content">
+                    <div class="mobile_menu_close open_mobile_menu">
+                        <i class="fal fa-times"></i>
+                    </div>
                     <div class="m-brand-logo">
                         <a href="index">
                             <img src="assets/images/logos/fashionvogue_colour_logo.png" alt="FashionVogue Logo">
                         </a>
                     </div>
-					<nav class="mobile-main-navigation  clearfix ul-li">
-						<ul id="m-main-nav" class="nav navbar-nav clearfix">
-							<li><a href="index">Home</a></li>
-							<li class="dropdown">
-								<a href="showverse">Showverse</a>
-								<ul class="dropdown-menu clearfix">
-									<li><a href="about">About</a></li>
-									<li><a href="experience">Experience</a></li>
-									<li><a href="highlights">Highlights</a></li>
-								</ul>
-							</li>
-							<li class="dropdown">
-								<a href="events">Events</a>
-								<ul class="dropdown-menu clearfix">
-									<li><a href="upcoming">Upcoming</a></li>
-									<li><a href="past-highlights">Past Highlights</a></li>
-								</ul>
-							</li>
-							<li class="dropdown">
-								<a href="opportunities">Opportunities</a>
-								<ul class="dropdown-menu clearfix">
-									<li><a href="models">Models</a></li>
-									<li><a href="designers">Designers</a></li>
-									<li><a href="makeup-artists">Makeup Artists</a></li>
-									<li><a href="sponsors">Sponsors</a></li>
-								</ul>
-							</li>
-							<li class="dropdown">
-								<a href="app">App</a>
-								<ul class="dropdown-menu clearfix">
-									<li><a href="download">Download</a></li>
-									<li><a href="features">Features</a></li>
-								</ul>
-							</li>
-							<li class="dropdown">
-								<a href="media">Media</a>
-								<ul class="dropdown-menu clearfix">
-									<li><a href="press">Press & Bytes</a></li>
-									<li><a href="gallery">Gallery</a></li>
-									<li><a href="blogs">Blogs</a></li>
-									<li><a href="contact">Get in Touch</a></li>
-								</ul>
-							</li>
-						</ul>
-					</nav>
-				</div>
-			</div>
-			<!-- /Mobile-Menu -->
-		</div>
-	</div>
+                    <nav class="mobile-main-navigation  clearfix ul-li">
+                        <ul id="m-main-nav" class="nav navbar-nav clearfix">
+                            <li><a href="index">Home</a></li>
+                            <li class="dropdown">
+                                <a href="showverse">Showverse</a>
+                                <ul class="dropdown-menu clearfix">
+                                    <li><a href="about">About</a></li>
+                                    <li><a href="experience">Experience</a></li>
+                                    <li><a href="highlights">Highlights</a></li>
+                                </ul>
+                            </li>
+                            <li class="dropdown">
+                                <a href="events">Events</a>
+                                <ul class="dropdown-menu clearfix">
+                                    <li><a href="upcoming">Upcoming</a></li>
+                                    <li><a href="past-highlights">Past Highlights</a></li>
+                                </ul>
+                            </li>
+                            <li class="dropdown">
+                                <a href="opportunities">Opportunities</a>
+                                <ul class="dropdown-menu clearfix">
+                                    <li><a href="models">Models</a></li>
+                                    <li><a href="designers">Designers</a></li>
+                                    <li><a href="makeup-artists">Makeup Artists</a></li>
+                                    <li><a href="sponsors">Sponsors</a></li>
+                                </ul>
+                            </li>
+                            <li class="dropdown">
+                                <a href="app">App</a>
+                                <ul class="dropdown-menu clearfix">
+                                    <li><a href="download">Download</a></li>
+                                    <li><a href="features">Features</a></li>
+                                </ul>
+                            </li>
+                            <li class="dropdown">
+                                <a href="media">Media</a>
+                                <ul class="dropdown-menu clearfix">
+                                    <li><a href="press">Press & Bytes</a></li>
+                                    <li><a href="gallery">Gallery</a></li>
+                                    <li><a href="blogs">Blogs</a></li>
+                                    <li><a href="contact">Get in Touch</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+            <!-- /Mobile-Menu -->
+        </div>
+    </div>
 </header><!-- /header -->
 
-<!-- Search PopUp -->
 
 <!-- Sidebar sidebar Item -->
 <div class="xs-sidebar-group info-group">
-	<div class="xs-overlay xs-bg-black">
-		<div class="row loader-area">
-			<div class="col-3 preloader-wrap">
-				<div class="loader-bg"></div>
-			</div>
-			<div class="col-3 preloader-wrap">
-				<div class="loader-bg"></div>
-			</div>
-			<div class="col-3 preloader-wrap">
-				<div class="loader-bg"></div>
-			</div>
-			<div class="col-3 preloader-wrap">
-				<div class="loader-bg"></div>
-			</div>
-		</div>
-	</div>
-	<div class="xs-sidebar-widget">
-		<div class="xs-sidebar-widget-container">
-			<div class="widget-heading">
-				<a href="#" class="close-side-widget">
-					X
-				</a>
-			</div>
-			<div class="sidebar-textwidget">
-
-				<div class="sidebar-info-contents headline pera-content">
-					<div class="content-inner">
-						<div class="logo">
-							<a href="index-1.html"><img src="assets/images/logos/fashionvogue_colour_logo.png" alt=""></a>
-						</div>
-						<div class="content-box">
-							<h5>About Us</h5>
-<p class="text">
-At FashionVogue, we believe first impressions are everything in the world of fashion and modeling. Our platform is designed to showcase your talent, style, and personality without distraction, allowing casting directors and agencies to focus on your unique look and potential. By creating a clean and professional presentation, we ensure your portfolio speaks for itself while keeping the spotlight exactly where it belongs—on you.
-</p>
-						</div>
-						<div class="gallery-box fashion-bubbles">
-    <h5>Gallery</h5>
-    <div class="bubble-gallery">
-        <div class="bubble"><img src="assets/images/events/model1.jpg" alt="Fashion Look 1"></div>
-        <div class="bubble"><img src="assets/images/events/model2.jpg" alt="Fashion Look 2"></div>
-        <div class="bubble"><img src="assets/images/events/model3.jpg" alt="Fashion Look 3"></div>
-        <div class="bubble"><img src="assets/images/events/model7.jpg" alt="Fashion Look 4"></div>
-        <div class="bubble"><img src="assets/images/events/model5.jpg" alt="Fashion Look 5"></div>
-        <div class="bubble"><img src="assets/images/events/model6.jpg" alt="Fashion Look 6"></div>
+    <div class="xs-overlay xs-bg-black">
+        <div class="row loader-area">
+            <div class="col-3 preloader-wrap">
+                <div class="loader-bg"></div>
+            </div>
+            <div class="col-3 preloader-wrap">
+                <div class="loader-bg"></div>
+            </div>
+            <div class="col-3 preloader-wrap">
+                <div class="loader-bg"></div>
+            </div>
+            <div class="col-3 preloader-wrap">
+                <div class="loader-bg"></div>
+            </div>
+        </div>
     </div>
-</div>
+    <div class="xs-sidebar-widget">
+        <div class="xs-sidebar-widget-container">
+            <div class="widget-heading">
+                <a href="#" class="close-side-widget">X</a>
+            </div>
+            <div class="sidebar-textwidget">
+                <div class="sidebar-info-contents headline pera-content">
+                    <div class="content-inner">
+                        <div class="logo">
+                            <a href="index"><img src="assets/images/logos/fashionvogue_colour_logo.png" alt=""></a>
+                        </div>
+                        <div class="content-box">
+                            <h5>About Us</h5>
+                            <p class="text">
+                                At FashionVogue, we believe first impressions are everything in the world of fashion and modeling. Our platform is designed to showcase your talent, style, and personality without distraction, allowing casting directors and agencies to focus on your unique look and potential. By creating a clean and professional presentation, we ensure your portfolio speaks for itself while keeping the spotlight exactly where it belongs—on you.
+                            </p>
+                        </div>
+                        <div class="gallery-box fashion-bubbles">
+                            <h5>Gallery</h5>
+                            <div class="bubble-gallery">
+                                <div class="bubble"><img src="assets/images/events/model1.jpg" alt="Fashion Look 1"></div>
+                                <div class="bubble"><img src="assets/images/events/model2.jpg" alt="Fashion Look 2"></div>
+                                <div class="bubble"><img src="assets/images/events/model3.jpg" alt="Fashion Look 3"></div>
+                                <div class="bubble"><img src="assets/images/events/model7.jpg" alt="Fashion Look 4"></div>
+                                <div class="bubble"><img src="assets/images/events/model5.jpg" alt="Fashion Look 5"></div>
+                                <div class="bubble"><img src="assets/images/events/model6.jpg" alt="Fashion Look 6"></div>
+                            </div>
+                        </div>
 
 <style>
 .fashion-bubbles .bubble-gallery {
@@ -181,7 +181,6 @@ At FashionVogue, we believe first impressions are everything in the world of fas
     gap: 15px;
     justify-items: center;
 }
-
 .fashion-bubbles .bubble {
     width: 90px;
     height: 90px;
@@ -190,7 +189,6 @@ At FashionVogue, we believe first impressions are everything in the world of fas
     border: 2px solid #fff;
     box-shadow: 0 3px 8px rgba(0,0,0,0.15);
 }
-
 .fashion-bubbles .bubble img {
     width: 100%;
     height: 100%;
@@ -198,22 +196,54 @@ At FashionVogue, we believe first impressions are everything in the world of fas
 }
 </style>
 
-						<div class="content-box">
-							<h5>Social Account</h5>
-							<ul class="social-box">
-								<li><a href="https://www.facebook.com/" class="fab fa-facebook-f"></a></li>
-								<li><a href="https://www.twitter.com/" class="fab fa-twitter"></a></li>
-								<li><a href="https://dribbble.com/" class="fab fa-dribbble"></a></li>
-								<li><a href="https://www.linkedin.com/" class="fab fa-linkedin"></a></li>
-							</ul>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
+                        <div class="content-box">
+                            <h5>Social Account</h5>
+                            <ul class="social-box">
+                                <li><a href="https://www.facebook.com/" class="fab fa-facebook-f"></a></li>
+                                <li><a href="https://www.twitter.com/" class="fab fa-twitter"></a></li>
+                                <li><a href="https://dribbble.com/" class="fab fa-dribbble"></a></li>
+                                <li><a href="https://www.linkedin.com/" class="fab fa-linkedin"></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 <!-- End of header section
-	============================================= -->
+    ============================================= -->
 
 
+<!-- ✅ Button styling -->
+<style>
+.login-btn {
+    background-color: orange;
+    color: #fff;
+    padding: 8px 18px;
+    border-radius: 5px;
+    margin-right: 15px;
+    font-size: 14px;
+    font-weight: 600;
+    text-decoration: none;
+    transition: all 0.3s ease;
+}
+.login-btn:hover {
+    background-color: #dc7c07ff;
+    color: #fff;
+}
+</style>
+
+<!-- ✅ JS to hide button if logged in -->
+<script>
+document.addEventListener("DOMContentLoaded", function() {
+    const loginBtn = document.getElementById("loginBtn");
+    const isLoggedIn = localStorage.getItem("isLoggedIn"); 
+
+    if (isLoggedIn === "true") {
+        if (loginBtn) loginBtn.style.display = "none";
+    } else {
+        if (loginBtn) loginBtn.style.display = "inline-block";
+    }
+});
+</script>
